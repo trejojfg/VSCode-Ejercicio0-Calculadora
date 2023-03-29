751d70f35a1673b935095491a9d28f4ec7a239d3
TREJO <129201545+trejojfg@users.noreply.github.com>
2023-03-29 14:17:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // paso 0: declarar variables.
+            // tipo de datos: int (enteros), float (numeros con ,), char (letras-caracteres), bool (verdadero-falso, 1-0)
+            int n1, n2, resultado;
+
+            // paso 1: pedir valores.
+            Console.WriteLine("Ingrese un número:");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro número:");
+            n2 = int.Parse(Console.ReadLine());
+
+            // paso 2: realizar calculo.
+            resultado = n1 + n2;
+
+            // paso 3: emitir resultado.
+            Console.WriteLine("El resultado es: " + resultado);
+            
+        }
+    }
+}
